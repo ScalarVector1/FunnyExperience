c8c31fd5e7d84ce28bbf12749ad5389d11775633
CollinHerber <rustaddicts@gmail.com>
2023-08-11 11:56:02 -0400

    feat: add alt function to sword types with 4 second cooldown

--- Content/Items/Gear/Weapons/Melee/Sword.cs
+++ Content/Items/Gear/Weapons/Melee/Sword.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using FunnyExperience.Core.Systems;
+using System.Linq;
+using Terraria.DataStructures;
 using Terraria.ID;
 
 namespace FunnyExperience.Content.Items.Gear.Weapons.Melee
@@ -6,20 +8,61 @@ namespace FunnyExperience.Content.Items.Gear.Weapons.Melee
 	internal class Sword : Gear
 	{
 		public override string Texture => $"{FunnyExperience.ModName}/Assets/Items/Gear/Weapon/Sword/Base";
-	
+
 		public override void SetDefaults()
 		{
 			Item.damage = 10;
-			Item.width = 40; 
+			Item.width = 40;
 			Item.height = 40;
-			Item.useStyle = ItemUseStyleID.Swing; 
-			Item.useTime = 20; 
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.useTime = 20;
 			Item.useAnimation = 20;
 			Item.autoReuse = true;
 			Item.DamageType = DamageClass.Melee;
 			Item.knockBack = 6;
 			Item.crit = 6;
-			Item.UseSound = SoundID.Item1; 
+			Item.UseSound = SoundID.Item1;
+			Item.shoot = ProjectileID.PurificationPowder;
+			Item.shootSpeed = 10f;
+		}
+
+		public override bool AltFunctionUse(Player player)
+		{
+			AltUseSystem modPlayer = player.GetModPlayer<AltUseSystem>();
+
+			// If cooldown is still active, do not allow alt usage.
+			if (modPlayer.altFunctionCooldown > 0)
+			{
+				Console.Write("Cannot use");
+				return false;
+			}
+
+			// Otherwise, set the cooldown and allow alt usage.
+			Console.Write("Can use");
+			modPlayer.altFunctionCooldown = 240;
+			return true;
+		}
+
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position,
+			Vector2 velocity, int type, int damage, float knockback)
+		{
+			if (player.altFunctionUse == 2)
+			{
+				Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
+			}
+
+			return false;
+		}
+
+		public override void ModifyShootStats(
+			Player player,
+			ref Vector2 position,
+			ref Vector2 velocity,
+			ref int type,
+			ref int damage,
+			ref float knockback)
+		{
+			type = ModContent.ProjectileType<Projectiles.LifeStealProjectile>();
 		}
 
 		public override string GenerateName()
---
+++ Core/Systems/AltUseSystem.cs
@@ -0,0 +1,18 @@
+﻿using FunnyExperience.Core.Systems.TreeSystem;
+using System.Linq;
+using Terraria.Audio;
+using Terraria.ModLoader.IO;
+
+namespace FunnyExperience.Core.Systems
+{
+	public class AltUseSystem : ModPlayer
+	{
+		public int altFunctionCooldown = 0;
+
+		public override void ResetEffects()
+		{
+			if (altFunctionCooldown > 0)
+				altFunctionCooldown--;
+		}
+	}
+}
